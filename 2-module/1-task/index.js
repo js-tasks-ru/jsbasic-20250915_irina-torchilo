db7dbaed98bb6da8884f8b1e1a563abedd6fcf43
torchiloirina-code <torchiloirina@gmail.com>
2025-09-29 19:53:14 +0300

    задание 2.1 решено

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (Number.isFinite(salaries[key])) {
+      sum += salaries[key];
+    }
+  }
+  return sum;
 }
+
+
